7143a2e772064ca87479eeb8e66d11b06bc45bcf
RJ <jensenrs@gmail.com>
2017-12-03 22:25:10 -0800

    preparing edit route for snacks

--- admin.js
+++ admin.js
@@ -27,11 +27,21 @@ function loadSnacks(baseURL){
     result.data.response.forEach(el => {
       adminTable.innerHTML += snackRow(el.id, el.name)
     })
+
     let deleteSnack = document.querySelectorAll('.delete-snack')
     for (var i = 0; i < deleteSnack.length; i++) {
       deleteSnack[i].addEventListener('click', (e) => {
         let thisId = e.target.getAttribute('data-id')
         destroySnack(thisId)
+      })
+    }
+
+    let editSnack = document.querySelectorAll('.edit-snack')
+    for (var i = 0; i < editSnack.length; i++) {
+      editSnack[i].addEventListener('click', (e) => {
+        let thisId = e.target.getAttribute('data-id')
+        console.log(thisId);
+        //open modal window with form to edit
       })
     }
   })
@@ -50,6 +60,17 @@ function destroySnack(id){
   return axios.delete(`${baseURL}/${id}`)
   .then(result => {
     loadSnacks('http://localhost:3000/api/snacks')
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
+function editSnack(id, body){
+  return axios.put(`${baseURL}/${id}`)
+  .then(result => {
+    //do stuff
+    console.log(result);
   })
   .catch(err => {
     console.log(err);
