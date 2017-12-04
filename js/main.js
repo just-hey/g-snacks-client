c7df7c649b047efe747485faff9d728f19acf6ab
Kevin Springer <KevinUSCU@outlook.com>
2017-12-04 15:33:08 -0800

    Add login code on page load. Add logout function.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// Connect to Heroku server
+// window.baseURL = 'https:// ???? .herokuapp.com'
+// Connect to local server
 window.baseURL = 'http://localhost:3000/api'
 
 window.navBar = document.querySelector('.navbar')
+window.snacksUser = null
+
+// Check for and get currently logged in user
+window.snacksUserToken = localStorage.getItem('snacksUserToken')
+// If user not logged in, redirect to main page
+if (!snacksUserToken && window.location.pathname !== '/index.html') window.location='index.html'
+// If user is logged in, get their user data
+if (snacksUserToken) {
+  axios.get(`${baseURL}/users/fromToken`, { headers: { authorization: `Bearer ${snacksUserToken}` } })
+  .then(result => { snacksUser = result.data.response } )
+}
+
+
+// Set navbar based on user state
+
+
+
+
+
+function logout() {
+  localStorage.removeItem('snacksUserToken')
+  snacksUserToken = null
+  window.location='index.html'
+}
+
