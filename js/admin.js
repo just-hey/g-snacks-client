e99c58758f373f0e3cddc31d8824afee1fed27a6
RJ <jensenrs@gmail.com>
2017-12-05 18:23:25 -0800

    custom form made

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,12 @@
 const usersURL = 'http://localhost:3000/api/users'
 const snacksURL = 'http://localhost:3000/api/snacks'
 
+let snackTab = document.querySelector('.snack-tab')
+let userTab = document.querySelector('.user-tab')
 let adminTable = document.querySelector('.admin-table')
 let adminTableHeader = document.querySelector('.admin-table-header')
 
-/////ADMIN TABS
-let snackTab = document.querySelector('.snack-tab')
-let userTab = document.querySelector('.user-tab')
-
+/////ADMIN TABS - toggle view and style
 snackTab.addEventListener('click', (e) => {
   if(!snackTab.classList.contains('active-tab')){
     snackTab.classList.add('active-tab')
@@ -26,6 +25,7 @@ userTab.addEventListener('click', (e) => {
 
 //////////LOAD USERS
 function loadUsers(usersURL){
+  //change table header
   adminTableHeader.innerHTML = ""
   adminTableHeader.innerHTML =
   `<tr>
@@ -33,7 +33,7 @@ function loadUsers(usersURL){
     <th>Username</th>
     <th>Admin</th>
   </tr>`
-
+  //placeholder html -!! will be dynamic
   adminTable.innerHTML = ""
   adminTable.innerHTML =
   `<tr>
@@ -41,100 +41,9 @@ function loadUsers(usersURL){
     <td>JanelleMonae</td>
     <td><i class="material-icons not-admin">account_box</i></td>
   </tr>
-  <tr>
-    <td scope="row"><i class="material-icons delete-user">close</i></td>
-    <td>LukeSkywalkerz</td>
-    <td><i class="material-icons not-admin">account_box</i></td>
-  </tr>
-  <tr>
-    <td scope="row"><i class="material-icons delete-user">close</i></td>
-    <td>Rey</td>
-    <td><i class="material-icons not-admin">account_box</i></td>
-  </tr>
-  <tr>
-    <td scope="row"><i class="material-icons delete-user">close</i></td>
-    <td>Sabine Wren</td>
-    <td><i class="material-icons not-admin">account_box</i></td>
-  </tr>
-  <tr>
-    <td scope="row"><i class="material-icons delete-user">close</i></td>
-    <td>Hera Syndulla</td>
-    <td><i class="material-icons not-admin">account_box</i></td>
-  </tr>
-  <tr>
-    <td scope="row"><i class="material-icons delete-user">close</i></td>
-    <td>Captain Phasma</td>
-    <td><i class="material-icons is-admin">account_box</i></td>
-  </tr>`
+  `
 }
 
-//////////LOAD SNACKS
-function loadAdminSnacks(snacksURL){
-  return axios.get(snacksURL)
-  .then(result => {
-    adminTableHeader.innerHTML = ""
-    adminTableHeader.innerHTML =
-    `<tr>
-      <th>Delete</th>
-      <th>Snack</th>
-      <th>Edit</th>
-    </tr>`
-
-    adminTable.innerHTML = ""
-    result.data.response.forEach(el => {
-      adminTable.innerHTML += snackRow(el.id, el.name)
-    })
-
-    let deleteSnack = document.querySelectorAll('.delete-snack')
-    for (var i = 0; i < deleteSnack.length; i++) {
-      deleteSnack[i].addEventListener('click', (e) => {
-        let thisId = e.target.getAttribute('data-id')
-        destroySnack(thisId)
-      })
-    }
-
-    let editSnack = document.querySelectorAll('.edit-snack')
-    for (var i = 0; i < editSnack.length; i++) {
-      editSnack[i].addEventListener('click', (e) => {
-        let thisId = e.target.getAttribute('data-id')
-        console.log(thisId);
-        //get associated info for this one snack
-        return getOneSnack(thisId) 
-        .then(result => {
-          //locate form inputs in DOM
-          let nameValue = document.querySelector('#editsnack-name')
-          let descriptionValue = document.querySelector('#editsnack-description')
-          let imageValue = document.querySelector('#editsnack-imageURL')
-
-          //add values
-          nameValue.setAttribute('value', result.name)
-          descriptionValue.setAttribute('value', result.description)
-          imageValue.setAttribute('value', result.img)
-
-          //listen for click on submit
-          let editSave = document.querySelector('#edit-save')
-          editSave.addEventListener('click', (e) => {
-
-            let body = {name: nameValue.value, description: descriptionValue.value, img: imageValue.value}
-            //bug: after editing one snack, then the edited values appear for every other snack that's clicked on.
-            axios.put(`${snacksURL}/${thisId}`, body)
-            .then(result => {
-              $('#editSnackModal').modal('hide')
-              loadAdminSnacks(snacksURL)
-            })
-            .catch(err => {
-              console.log(err);
-            })
-            //and then... how to close the modal?
-          })
-
-        })
-      })
-    }
-  })
-  .catch((err) => {console.log(err)})
-}
-loadAdminSnacks(snacksURL)
 
 function snackRow(id, name){
   return `<tr data-id="${id}">
@@ -144,6 +53,88 @@ function snackRow(id, name){
   </tr>`
 }
 
+//////////LOAD SNACKS
+function loadAdminSnacks(snacksURL){
+  return axios.get(snacksURL)
+  .then(result => {
+    //change table header
+    adminTableHeader.innerHTML = ""
+    adminTableHeader.innerHTML =
+    `<tr>
+      <th>Delete</th>
+      <th>Snack</th>
+      <th>Edit</th>
+    </tr>`
+
+    //add rows
+    adminTable.innerHTML = ""
+    result.data.response.forEach(el => {
+      adminTable.innerHTML += snackRow(el.id, el.name)
+    })
+
+    //LISTEN for delete click
+    //!! should add "are you sure?" messaging
+    let deleteSnack = document.querySelectorAll('.delete-snack')
+    for (var i = 0; i < deleteSnack.length; i++) {
+      deleteSnack[i].addEventListener('click', (e) => {
+        let thisId = e.target.getAttribute('data-id')
+        destroySnack(thisId)
+      })
+    }
+
+    //LISTEN for edit click
+    let editSnack = document.querySelectorAll('.edit-snack')
+    for (var i = 0; i < editSnack.length; i++) {
+      editSnack[i].addEventListener('click', (e) => {
+        let thisId = e.target.getAttribute('data-id')
+        console.log(thisId);
+        //get associated info for this one snack
+        return getOneSnack(thisId)
+        .then(result => {
+
+          let editFormDiv = document.querySelector('.edit-form')
+          let adminTableContainer = document.querySelector('.admin-container')
+
+          adminTableContainer.innerHTML = ""
+          editFormDiv.innerHTML = editSnackForm(result.id, result.name, result.description, result.img)
+
+
+          // //MDB MODAL
+          // //locate form inputs in DOM
+          // let nameValue = document.querySelector('#editsnack-name')
+          // let descriptionValue = document.querySelector('#editsnack-description')
+          // let imageValue = document.querySelector('#editsnack-imageURL')
+          //
+          // //add values
+          // nameValue.setAttribute('value', result.name)
+          // descriptionValue.setAttribute('value', result.description)
+          // imageValue.setAttribute('value', result.img)
+          //
+          // //listen for click on submit
+          // let editSave = document.querySelector('#edit-save')
+          // editSave.addEventListener('click', (e) => {
+          //
+          //   let body = {name: nameValue.value, description: descriptionValue.value, img: imageValue.value}
+          //   //bug: after editing one snack, then the edited values appear for every other snack that's clicked on.
+          //   axios.put(`${snacksURL}/${thisId}`, body)
+          //   .then(result => {
+          //     $('#editSnackModal').modal('hide')
+          //     loadAdminSnacks(snacksURL)
+          //   })
+          //   .catch(err => {
+          //     console.log(err);
+          //   })
+          // })
+
+        })
+      })
+    }
+  })
+  .catch((err) => {console.log(err)})
+}
+loadAdminSnacks(snacksURL)
+
+//////////GET ONE SNACK
 function getOneSnack(id){
   return axios.get(`${snacksURL}/${id}`)
   .then(result => {
@@ -152,6 +143,7 @@ function getOneSnack(id){
   .catch((err) => {console.log(err)})
 }
 
+//////////DELETE ONE SNACK
 function destroySnack(id){
   return axios.delete(`${snacksURL}/${id}`)
   .then(result => {
@@ -163,6 +155,8 @@ function destroySnack(id){
   })
 }
 
+//////////EDIT ONE SNACK
+//not currently being used !!
 function editSnack(id, body){
   return axios.put(`${snacksURL}/${id}`, body)
   .then(result => {
