fca136221d3a6edbaf6b72c4db18056cd4cf29d7
RJ <jensenrs@gmail.com>
2017-12-08 08:10:22 -0800

    can click review stars and see the appropriate amount filled in

--- js/templates/add-review.js
+++ js/templates/add-review.js
@@ -3,11 +3,11 @@ function addReviewForm(){
   return `<form>
     <textarea name="review-text" id="review-text" placeholder="What do you think?"></textarea>
     <p class="review-stars">
-      <i class="material-icons star one-star">star_border</i>
-      <i class="material-icons star two-star">star_border</i>
-      <i class="material-icons star three-star">star_border</i>
-      <i class="material-icons star four-star">star_border</i>
-      <i class="material-icons star five-star">star_border</i>
+      <i class="material-icons star" data-star="1">star_border</i>
+      <i class="material-icons star" data-star="2">star_border</i>
+      <i class="material-icons star" data-star="3">star_border</i>
+      <i class="material-icons star" data-star="4">star_border</i>
+      <i class="material-icons star" data-star="5">star_border</i>
     </p>
     <div class="save-cancel-buttons mt-4">
       <button class="cancel">Cancel</button>
