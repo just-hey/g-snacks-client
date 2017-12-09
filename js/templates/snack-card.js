161fa15f1a01645723b096f88880da3a9d633013
Kevin Springer <KevinUSCU@outlook.com>
2017-12-08 16:02:40 -0800

    Shorten description for cards.

--- js/templates/snack-card.js
+++ js/templates/snack-card.js
@@ -1,7 +1,16 @@
 function snackCard(id, name, description, img){
-  return `<div class="col-lg-4 mb-4" data-id="${id}">
+    // shorten description
+    let shortDescription = description
+    if (shortDescription.length > 100) {
+        shortDescription = shortDescription.substring(0, 100)
+        let lastSpace = shortDescription.lastIndexOf(' ')
+        if (lastSpace > 0) shortDescription = shortDescription.substring(0, lastSpace)
+        shortDescription = shortDescription + `<a href="${baseURL}/${id}"> ...(more)</a>`
+    }
+    // return template
+    return `<div class="col-lg-4 mb-4">
             <!--Card-->
-            <div class="card wow fadeIn" data-wow-delay="0.6s">
+            <div data-id="${id}" class="card wow fadeIn" data-wow-delay="0.6s">
                 <!--Card image-->
                 <div class="view overlay hm-zoom" style="display: flex; align-items: center; overflow: hidden; height: 180px;">
                     <img class="img-fluid" src="${img}" alt="Image of snack">
@@ -11,7 +20,7 @@ function snackCard(id, name, description, img){
                     <!--Title-->
                     <h4 class="card-title"><a data-id="${id}" class="snackLink" href="${baseURL}/${id}">${name}</a></h4>
                     <!--Text-->
-                    <p class="card-text" style="overflow: scroll; max-height: 60px">${description}</p>
+                    <p class="card-text">${shortDescription}</p>
                     <!--Stars-->
                     <p>
                       <i class="material-icons star">star</i>
